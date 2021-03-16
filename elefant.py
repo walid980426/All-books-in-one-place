1d1478c545f9580e2d6eccaaa4384a5bb254018f
sztodo <mihastodo@yahoo.com>
2021-03-16 20:18:06 +0200

    Update elefant.py

--- elefant.py
+++ elefant.py
@@ -1,15 +1,13 @@
 from selenium import webdriver
 
-geckodriver = r'geckodriver.exe'
-driver = webdriver.Firefox(executable_path=geckodriver)
-driver.get("https://www.elefant.ro/list/carti/carte")
+driver = webdriver.Firefox()
 urlSource = "https://www.elefant.ro/list/carti/carte?pag="
-i = 515
+i = 1
 while True:
     driver.get(urlSource+str(i))
-    for line in driver.find_element_by_xpath("//div[contains(., 'product-image-container')]").find_elements_by_xpath('//a'):
-        '''page = driver.find_element_by_xpath('//*[@id="family-page"]/div/div[1]/div[2]/div[5]/div')'''
-        pass
+    for line in driver.find_elements_by_xpath('//div[5]/div/div/div/a'):
+        line.click()
+        driver.implicitly_wait(3)
     i += 1
     if len(driver.find_elements_by_xpath("//div[2]/div[5]/div")) > 0:
         continue
