5cdd6913b9ee942ce37cdf6deb8bc141dbd27d74
sztodo <mihastodo@yahoo.com>
2021-03-30 18:56:59 +0300

    extragere pret, imagine, detalii

--- elefant/elefant.py
+++ elefant/elefant.py
@@ -7,6 +7,7 @@ i = 1
 data = {}
 data['price'] = []
 data['details'] = []
+data['img'] = []
 try:
     while True:
         driver.get(urlSource+str(i))
@@ -14,13 +15,19 @@ try:
         for line in driver.find_elements_by_xpath('//div[@class="product-image-container"]'):
             browser = webdriver.Firefox()
             elem = line.find_element_by_tag_name("a")
-            # driver.execute_script("arguments[0].setAttribute('target','_blank')", line.find_element_by_tag_name("a"))
             link = elem.get_attribute("href")
             browser.get(link)
             browser.implicitly_wait(3)
             pret_actual = browser.find_element_by_css_selector(".pdp-table-th > .current-price")
-
-            print(pret_actual)
+            pret_actual = pret_actual.text
+            data['price'].append(pret_actual)
+            image_link = browser.find_element_by_css_selector('.product-thumb-set > .product-image')
+            image_link = image_link.get_attribute('src')
+            data['img'].append(image_link)
+            detalii = browser.find_element_by_xpath('//*[@id]/div/dl')
+            detalii = detalii.text
+            data['details'].append(detalii)
+            print(data)
             browser.quit()
         i += 1
         if len(driver.find_elements_by_xpath("//div[2]/div[5]/div")) > 0:
