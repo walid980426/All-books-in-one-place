b5fb745c73b3953c37a85ea64bf31b5294723aa0
DESKTOP-7FLOC5V\KyleYG <iseby29@gmail.com>
2021-04-12 16:24:27 +0300

    Finalizare preluare date si stocarea acestora in json

--- Aleph/Aleph.py
+++ Aleph/Aleph.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from time import sleep
+from ToJson import to_json
 driver = webdriver.Firefox()
 
 i=1
+id=1
 while True:
     driver.get("http://aleph.bcucluj.ro:8991/F/U37IYB6QYV145YC3L2RLHFD699HAK9UK768M46UDUA8H5QDBDC-36579?func=find-c-0")
     driver.find_element_by_xpath("//input[@name='ccl_term']").send_keys("SYS="+str(i))
@@ -12,14 +14,34 @@ while True:
 
         sleep(5)
     except:
+        list= []
         sleep(2)
         driver.find_element_by_xpath("/html/body/table[4]/tbody/tr[2]/td[4]/a").click() #carte
         sleep(2)
         driver.find_element_by_xpath("/html/body/table[3]/tbody/tr/td[5]/a").click() #etichete
         sleep(2)
-        data = driver.find_element_by_xpath("//tbody[contains(., '001')]")#elemnte Etichete MARC
-        for line in data.find_elements_by_xpath('//td'):
-            print(line.get_attribute('innerHTML'))
+        data = driver.find_element_by_xpath("//html//body//table[5]")#elemnte Etichete MARC
+        for line in data.find_elements_by_xpath('//tbody//tr//td'):
+            list.append(line.text)
+        k=0
+        for k in range (len(list)):
+            if list[k]=="FMT":
+                break
+        list=list[k:]
+        k=0
+        for k in range (len(list)):
+            if list[k]=="SYS":
+                break
+        list=list[:k+2]
+        to_json(list,id)
+        id=id+1
+
+        print(list)
+
+
+
+
+
 
 
     finally:
