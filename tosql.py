0e121702d76116ff1892cfff7bdfede53463a70c
Tibi <andercoiosif@gmail.com>
2021-04-19 15:34:12 +0300

    libris

--- tosql.py
+++ tosql.py
@@ -61,21 +61,21 @@ def databaseLibris():
                 mydb.commit()
 
 
-# file = open("data/tinread.json","r")
-# jsonfile=json.load(file)
-# nume = jsonfile[1][str(2)][0]['fld']['fld_name']
-# index_1=jsonfile[1][str(2)][0]['fld']['ind_1']
-# index_2=jsonfile[1][str(2)][0]['fld']['ind_2']
-# valore=jsonfile[1][str(2)][0]['fld']['val']
-# # for i in jsonfile[1][str(2)][0]['fld']['subflds']:
-# #   print(i)
-# sql = "INSERT INTO carte (bibloteca_id) VALUES (1)"
-# mycursor.execute(sql)
-# mydb.commit()
-# sql = f"INSERT INTO fields (carte_id,valore,nume) VALUES (1,'{valore}','{nume}')"
-# mycursor.execute(sql)
-#
-# mydb.commit()
-# print(mycursor.rowcount, "record inserted.")
+file = open("data/tinread.json","r")
+jsonfile=json.load(file)
+nume = jsonfile[1][str(2)][0]['fld']['fld_name']
+index_1=jsonfile[1][str(2)][0]['fld']['ind_1']
+index_2=jsonfile[1][str(2)][0]['fld']['ind_2']
+valore=jsonfile[1][str(2)][0]['fld']['val']
+# for i in jsonfile[1][str(2)][0]['fld']['subflds']:
+#   print(i)
+sql = "INSERT INTO carte (bibloteca_id) VALUES (1)"
+mycursor.execute(sql)
+mydb.commit()
+sql = f"INSERT INTO fields (carte_id,valore,nume) VALUES (1,'{valore}','{nume}')"
+mycursor.execute(sql)
 
-#jsonfile[i][str(i+1)][index]['fld']['fld_name']
+mydb.commit()
+print(mycursor.rowcount, "record inserted.")
+
+jsonfile[i][str(i+1)][index]['fld']['fld_name']
