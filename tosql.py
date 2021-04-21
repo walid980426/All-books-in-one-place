83d53812ee2caa8fe855e8902e88baa8f8c93991
Walid <Mohammad.Kh.Walid@utcluj.didatec.ro>
2021-04-21 11:24:01 +0300

    add date in bd in tinread.json

--- tosql.py
+++ tosql.py
@@ -7,19 +7,18 @@ username = 'all-books-in-one-place'
 password = 'gRAyL3QShEFeruwv'
 port = '23306'
 
-
 mydb = mysql.connector.connect(host=server, user=username, password=password, port=port, database=database)
 
 mycursor = mydb.cursor()
 
 
 def databasealeph():
-    file = open("data/Aleph.json","r")
-    jsonfile=json.load(file)
+    file = open("data/Aleph.json", "r")
+    jsonfile = json.load(file)
     nume = jsonfile[1][str(2)][0]['fld']['fld_name']
-    index_1=jsonfile[1][str(2)][0]['fld']['ind_1']
-    index_2=jsonfile[1][str(2)][0]['fld']['ind_2']
-    valore=jsonfile[1][str(2)][0]['fld']['val']
+    index_1 = jsonfile[1][str(2)][0]['fld']['ind_1']
+    index_2 = jsonfile[1][str(2)][0]['fld']['ind_2']
+    valore = jsonfile[1][str(2)][0]['fld']['val']
     # for i in jsonfile[1][str(2)][0]['fld']['subflds']:
     #   print(i)
     sql = "INSERT INTO carte (bibloteca_id) VALUES (1)"
@@ -30,7 +29,6 @@ def databasealeph():
 
     mydb.commit()
     print(mycursor.rowcount, "record inserted.")
-
 
     pass
 
@@ -57,25 +55,49 @@ def databaseLibris():
         else:
             for i in value:
                 # actualizam fiecare field in tabelul fields
-                mycursor.execute(f"""UPDATE field SET value="{i['fld']['val']}" WHERE carte_id={exista[0]} AND field="{i['fld']['fld_name']}" """)
+                mycursor.execute(
+                    f"""UPDATE field SET value="{i['fld']['val']}" WHERE carte_id={exista[0]} AND field="{i['fld']['fld_name']}" """)
                 mydb.commit()
 
 
-file = open("data/tinread.json","r")
-jsonfile=json.load(file)
-nume = jsonfile[1][str(2)][0]['fld']['fld_name']
-index_1=jsonfile[1][str(2)][0]['fld']['ind_1']
-index_2=jsonfile[1][str(2)][0]['fld']['ind_2']
-valore=jsonfile[1][str(2)][0]['fld']['val']
-# for i in jsonfile[1][str(2)][0]['fld']['subflds']:
-#   print(i)
-sql = "INSERT INTO carte (bibloteca_id) VALUES (1)"
-mycursor.execute(sql)
-mydb.commit()
-sql = f"INSERT INTO fields (carte_id,valore,nume) VALUES (1,'{valore}','{nume}')"
-mycursor.execute(sql)
+def databasetinread():
+    file = open("data/tinread.json", "r")
+    jsonfile = json.load(file)
+    for i in range(len(jsonfile)):
+        identif = jsonfile[i]['1'][1]['fld']['val']
 
-mydb.commit()
-print(mycursor.rowcount, "record inserted.")
+        mycursor.execute(f"INSERT INTO  resource (id_library,identifier) VALUES (1,{identif})")
+        mydb.commit()
+        mycursor.execute(f"SELECT id FROM resource WHERE id_library=1 AND identifier='{identif}'")
+        id_resource = mycursor.fetchall()
+        id_resource=id_resource[0][0]
+        for j in range(len(jsonfile[i])):
+            nume = jsonfile[i]['1'][j]['fld']['fld_name']
+            index_1 = jsonfile[i]['1'][j]['fld']['ind_1']
+            if (index_1 == ""):
+                index_1 = "-1"
 
-jsonfile[i][str(i+1)][index]['fld']['fld_name']
+            index_2 = jsonfile[i]['1'][j]['fld']['ind_2']
+            if (index_2 == ""):
+                index_2 = "-1"
+            valore = jsonfile[i]['1'][j]['fld']['val']
+            if (valore == ""):
+                valore = "#"
+            mycursor.execute(
+                f"INSERT INTO field (field.id_resource,field.value ,field.index_1,field.index_2,field.field) VALUES ({id_resource},'{valore}','{index_1}','{index_2}','{nume}')")
+            mydb.commit()
+            mycursor.execute(f"SELECT id FROM field WHERE field.id_resource= '{id_resource}' and field.value ='{valore}' and field.field='{nume}'and field.index_1 ='{index_1}'and field.index_2 ='{index_2}'")
+            id_field = mycursor.fetchall()
+            if(id_field):
+                id_field=id_field[0][0]
+                for k in range(len(jsonfile[i]['1'][j]['fld']['subflds'])):
+                    nume = jsonfile[i]['1'][j]['fld']['subflds'][k]['subfld']['name']
+                    val = jsonfile[i]['1'][j]['fld']['subflds'][k]['subfld']['val']
+                    mycursor.execute(
+                        f"INSERT INTO subfield (id_field,'name','value') VALUES ('{id_field}','{nume}','{val}'")
+                    mydb.commit()
+
+
+    # jsonfile[i][str(i+1)][index]['fld']['fld_name']
+
+
